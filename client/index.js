930c813f62e713be68bbeb7e8fa12381ae13afaf
rivari22 <muchammadrvr22@gmail.com>
2020-10-30 00:40:55 +0700

    oauth google sign in

--- client/index.js
+++ client/index.js
@@ -1,28 +1,59 @@
 const serverSpace = "http://localhost:3000/spaceX"
 const SERVER = "http://localhost:3000"
 
-
 $(document).ready(() => {
     const token = localStorage.getItem('token');
     if (token) {
-        $("#login-page").hide()
-        $("#homepage").show()
-        $("#register").hide()
-        $("#section-rocket").show()
-        $("#section-space").show()
-        $("#jumbotron").show()
-        $("#footer").show()
+        afterLogin()
         renderLaunches()
+        
+        //testing login berhasil dan dpt token ***
+        $(".testing-login").append(`<p>token: ${token}</p>`).show()
+        //testing diatas, adalah login berhasil dan dpt token
     } else {
-        $("#login-page").show()
-        $("#homepage").hide()
-        $("#register").hide()
-        $("#section-rocket").hide()
-        $("#section-space").hide()
-        $("#jumbotron").hide()
-        $("#footer").hide()
+        homeBeforeLogin()
+        renderLaunches()
     }
 })
+
+//navigate
+$(".home-logo").on("click", () => {
+    homeBeforeLogin()
+})
+
+$(".btn-login").on("click", () => {
+    $("#login-page").show()
+    $("#homepage").hide()
+    $("#register").hide()
+    $("#section-rocket").hide()
+    $("#section-space").hide()
+    $("#jumbotron").hide()
+    $("#footer").hide()
+})
+
+$(".btn-register").on("click", () => {
+    $("#login-page").hide()
+    $("#homepage").hide()
+    $("#register").show()
+    $("#section-rocket").hide()
+    $("#section-space").hide()
+    $("#jumbotron").hide()
+    $("#footer").hide()
+})
+//end navigate
+
+function homeBeforeLogin() {
+    $("#login-page").hide()
+    $("#homepage").show()
+    $("#register").hide()
+    $("#section-rocket").show()
+    $("#section-space").show()
+    $("#jumbotron").show()
+    $("#footer").show()
+    $("#nav-logout").hide()
+    $("#nav-login").show()
+    $("#nav-register").show()
+}
 
 function registerPage() {
     $("#login-page").hide()
@@ -36,73 +67,86 @@ function registerPage() {
 
 function register(e) {
     e.preventDefault();
-        const first_name = $('#firstName').val();
-        const last_name = $('#lastName').val();
-        const email = $('#register-email').val();
-        const password = $('#register-password').val();
-        const birth_date = $('#birthDate').val();
-        $.ajax({
-            method: "POST",
-            url: SERVER + "/auth/register",
-            data: {
-                first_name,
-                last_name,
-                email,             
-                password,                
-                birth_date
-            }
-        })
-        .done(response => {
-            console.log(response);
-        })
-        .fail(err => {
-            console.log(err);
-        });
+    const first_name = $('#firstName').val();
+    const last_name = $('#lastName').val();
+    const email = $('#register-email').val();
+    const password = $('#register-password').val();
+    const birth_date = $('#birthDate').val();
+    $.ajax({
+        method: "POST",
+        url: SERVER + "/auth/register",
+        data: {
+            first_name,
+            last_name,
+            email,             
+            password,                
+            birth_date
+        }
+    })
+    .done(response => {
+        // console.log(response);
+    })
+    .fail(err => {
+        console.log(err);
+    });
 }
 
 function login(e) {
     e.preventDefault();
-        const email = $('#email').val();
-        const password = $('#password').val();
-        console.log(email, password);
-        $.ajax({
-            method: "POST",
-            url: SERVER + "/auth/login",
-            data: {
-                email :email,
-                password : password
-            }
-        })
-        .done(response => {
-            const token = response.access_token;
-            localStorage.setItem('token', token);
-            renderLaunches()
-            $("#login-page").hide()
-            $("#homepage").show()
-            $("#register").hide()
-            $("#section-rocket").show()
-            $("#section-space").show()
-            $("#jumbotron").show()
-            $("#footer").show()
-        })
-        .fail(err => {
-            console.log(err);
-        });
+    const email = $('#email').val();
+    const password = $('#password').val();
+    // console.log(email, password);
+    $.ajax({
+        method: "POST",
+        url: SERVER + "/auth/login",
+        data: {
+            email :email,
+            password : password
+        }
+    })
+    .done(response => {
+        const token = response.access_token;
+        // console.log(token)
+        localStorage.setItem('token', token);
+        // renderLaunches()
+        $("#login-page").hide()
+        $("#homepage").show()
+        $("#register").hide()
+        $("#section-rocket").show()
+        $("#section-space").show()
+        $("#jumbotron").show()
+        $("#footer").show()
+    })
+    .fail(err => {
+        console.log(err);
+    });
 }
 
 function logout() {
-    $("#login-page").show()
-    $("#homepage").hide()
-    $("#register").hide()
-    $("#section-rocket").hide()
-    $("#section-space").hide()
-    $("#jumbotron").hide()
-    $("#footer").hide()
     $('#email').val('')
     $('#password').val('')
     localStorage.removeItem('token');
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+    homeBeforeLogin()
 }
 
+function afterLogin() {
+    $("#login-page").hide()
+    $("#homepage").show()
+    $("#register").hide()
+    $("#section-rocket").show()
+    $("#section-space").show()
+    $("#jumbotron").show()
+    $("#footer").show()
+    $("#nav-logout").show()
+    $("#nav-login").hide()
+    $("#nav-register").hide()
+}
+
+// render table launches upcoming rockets
 function renderLaunches() {
     let customer 
     let rockets
@@ -121,9 +165,7 @@ function renderLaunches() {
             rocketId = result.rocket
             getCustomerLaunches(result.payloads[0])
             .done(({customers}) => {
-                // console.log(test.customers[0])
                 customer = customers
-               
                 // console.log(customer)
                 getRocketLaunches(rocketId)
                 .done(rocket => {
@@ -176,3 +218,34 @@ function getLaunchpad(launchpadId) {
         url: serverSpace +`/launchpads/${launchpadId}`,
     })
 }
+// render table launches upcoming rockets <- until here 
+
+// google oauth
+//sign in
+function onSignIn(googleUser) {
+    const google_token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method: "POST",
+        url: SERVER + "/auth/googleLogin",
+        data:{
+            google_access_token: google_token
+        }
+    })
+    .done(response => {
+        console.log(response, "ni token")
+        localStorage.setItem('token', response);
+        afterLogin()
+    })
+    .fail(err => console.log(err))
+}
+
+//sign out
+function signOut() {
+    const auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+
+  
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,5 +3,6 @@ const AuthController = require('../controllers/authController');
 
 router.post('/register', AuthController.register);
 router.post('/login', AuthController.login);
+router.post('/googleLogin', AuthController.googleLogin);
 
 module.exports = router;
