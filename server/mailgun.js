46cce61a9fe50875d4c4ce2fc2685d6b09899530
HarisPratama <hpratama324@gmail.com>
2020-10-29 15:05:35 +0700

    1. Private domain key and api key

--- server/mailgun.js
+++ server/mailgun.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const emailRivari = 'muchammadrvr22@gmail.com';
 const emailFauza = 'umardastan2014@gmail.com';
 const emailDicky = 'dickyundead@gmail.com';
@@ -5,8 +6,8 @@ const emailHaris = 'hpratama324@gmail.com';
 
 
 const mailgun = require("mailgun-js");
-const api_key = 'aa68f07b8b6533dc71e8ed1818ee4bdb-9b1bf5d3-80d4f39d';
-const DOMAIN = 'sandbox36909680ff4e4c77bca23cba73f416f2.mailgun.org';
+const api_key = process.env.APIKEY;
+const DOMAIN =  process.env.DOMAIN;
 const mg = mailgun({apiKey: api_key, domain: DOMAIN});
 const data = [
     {
